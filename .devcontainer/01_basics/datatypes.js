2a7d8744856b26b4887d8a52c7e7070b7539dc87
anayan-05 <anayan0915@gmail.com>
2025-01-25 12:52:22 +0000

    done

---
+++ .devcontainer/01_basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3) // we are using nodejs and mot browser
+
+let name="Anayan"
+let age= 18
+let isLoggedIn= false
+let state;
+// number => 2 to the power 53
+// bigint
+// string =>""
+// boolean => true/false
+// null=> standalone value
+// undefined => 
+// symbol=> unique
+
+
+//object
+
+console.log(typeof null); // object
---
+++ .devcontainer/01_basics/variables.js
@@ -0,0 +1,17 @@
+const accountId= 144553
+let accountEmail="anayan0915@gmail.com"
+var accountPassword="123456"
+accountCity= "Jaipur"
+let accountState;
+
+
+// accountId=2 //not allowed
+accountEmail="hc@gmail.com"
+accountPassword="234567"
+accountCity="kashmir"
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+/*
+prefer not to use var 
+because of issue in block scope and functions
+*/
